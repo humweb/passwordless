9d68c48f42822e76e688bfe7cafe7e663be0ca5c
ryun <ryun@users.noreply.github.com>
2023-06-16 10:18:21 +0000

    Fix styling

--- src/Auth/SentinelDriver.php
+++ src/Auth/SentinelDriver.php
@@ -5,7 +5,6 @@ namespace Humweb\Passwordless\Auth;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use LGL\Auth\Laravel\Facades\Sentinel;
 
-
 class SentinelDriver extends Driver
 {
     public function login(AuthenticatableContract $user)
